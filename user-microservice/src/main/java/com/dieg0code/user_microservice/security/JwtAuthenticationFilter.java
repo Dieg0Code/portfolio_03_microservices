9c34748e6b61c9a0320aca3d5f455a9ce1581e72
Dieg0Code <diegoobando20@gmail.com>
2024-08-22 20:29:14 -0400

    ????

--- user-microservice/src/main/java/com/dieg0code/user_microservice/security/JwtAuthenticationFilter.java
+++ user-microservice/src/main/java/com/dieg0code/user_microservice/security/JwtAuthenticationFilter.java
@@ -12,11 +12,13 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.List;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Value("${JWT_SECRET_KEY}")
